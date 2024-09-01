9854114b93e8110a9643749dc09a11c91c6bc0ae
林林 <812568734@qq.com>
2024-09-01 10:21:06 +0800

    Update foot.php

--- compoment/foot.php
+++ compoment/foot.php
@@ -341,6 +341,8 @@ if (typeof Prism !== 'undefined') {
     </script>
 <?php endif; ?>
 <?php if(Bsoptions('CustomizationFooterJsCode')): ?><?php echo Bsoptions('CustomizationFooterJsCode'); ?><?php endif; ?>
+<!-- Use Instant Page -->
+<script src="//instant.page/5.2.0" type="module" integrity="sha384-jnZyxPjiipYXnSU0ygqeac2q7CVYMbh84q0uHVRRxEtvFPiQYbXWUorga2aqZJ0z"></script>
 </body>
 </html>
 <?php if(Bsoptions('Compress') == true) :?>
